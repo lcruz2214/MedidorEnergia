8a832e286ecb0699e3469d3789de18bb829e77f0
Leonardo Cruz <lcruz2214@gmail.com>
2019-07-21 22:39:32 -0300

    primeira versão do codigo, proximo passo separar as funções

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-import request
+import requests
+
+dado = None
+
+try:
+    dado = requests.get("https://api.thingspeak.com/channels/814858/feeds.json?results=2")
+    dado = dado.json()
+    
+    print(dado["feeds"][0]['created_at']) # recebendo valor de data ... será tratado posteriormente
+    print(dado["feeds"][0]['field1']) # valor de corrente de ventiladores
+    print(dado["feeds"][0]['field2']) # valor de corrente de iluminção
+    
+    for a in dado["feeds"][0]['created_at']: # for como teste para separação de ano, mes, dia e hh:mm:ss
+        print(a)
+ 
+except:
+    print(dado.status_code)
+
+''' 
+    criar função para request 
+    criar função para separar datas
+    criar função para escrever no excel
+'''
